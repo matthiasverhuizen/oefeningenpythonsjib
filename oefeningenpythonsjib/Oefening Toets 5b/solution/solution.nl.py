7d075a5a34bb57bce6377b17cc2b8f7f20e30020
oefeningenpythonsjib <114345894+matthiasverhuizen@users.noreply.github.com>
2025-05-06 18:59:31 +0200

    t

--- oefeningenpythonsjib/Oefening Toets 5b/solution/solution.nl.py
+++ oefeningenpythonsjib/Oefening Toets 5b/solution/solution.nl.py
@@ -6,5 +6,5 @@ interest = int(input("Voer de jaarlijkse interest in: "))
 jaar = 0
 while bedrag < 2500:
     bedrag *= (100 + interest) / 100
-    print(jaar, round(bedrag))
     jaar = jaar + 1
+    print(jaar, round(bedrag))
