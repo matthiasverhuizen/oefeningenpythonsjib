84a468a50e8cdfa8f72554889628e830d0a842fd
oefeningenpythonsjib <114345894+matthiasverhuizen@users.noreply.github.com>
2025-05-06 19:11:34 +0200

    t

--- oefeningenpythonsjib/Oefening Toets 5b/solution/solution.nl.py
+++ oefeningenpythonsjib/Oefening Toets 5b/solution/solution.nl.py
@@ -1,10 +1,10 @@
 # Vraag de gebruiker om het startbedrag en de jaarlijkse interest
-bedrag = int(input("Voer het startbedrag in: "))
+basis = int(input("Voer het startbedrag in: "))
 interest = int(input("Voer de jaarlijkse interest in: "))
 
 # Bereken het bedrag op de rekening na elk jaar
 jaar = 0
 while bedrag < 2500:
-    bedrag *= (100 + interest) / 100
+    basis = basis(100 + interest) / 100
     jaar = jaar + 1
-    print(jaar, round(bedrag))
+    print(jaar, round(basis))
